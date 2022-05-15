8a0096764f62f8bfdb46d9be73af79f009835d5d
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-05-15 22:46:34 +0100

    Added ability to filter products

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from audioop import reverse
 from django.shortcuts import redirect, render, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Store
+from .models import Store, Category
 
 # Create your views here.
 
@@ -11,8 +11,14 @@ def all_products(request):
 
     products = Store.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,6 +31,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'store/store.html', context)
