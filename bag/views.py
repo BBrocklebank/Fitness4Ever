91cbd000fa5f53a27e4e1f5ed7c501ce9f134d5d
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-06-07 23:55:35 +0100

    Feat: Created add to bag view and form functionality

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_bag(request):
     ''' View, returns shopping bag page '''
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """Add specified quantity of item to bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    #Checks for bag session variable, initialises one if not found (python dictionary)
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
