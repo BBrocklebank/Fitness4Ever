28da52b0fa45aa825b71b6ddcdbb0378c3c92d27
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-07-06 19:09:18 +0100

    Feat: Completed profile view, added order history

--- fitness4ever/settings.py
+++ fitness4ever/settings.py
@@ -181,7 +181,7 @@ FREE_DELIVERY_THRESHOLD = 40
 STANDARD_DELIVERY_PERCENTAGE = 7.5
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
-STRIPE_SECRET_KEY = env('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
