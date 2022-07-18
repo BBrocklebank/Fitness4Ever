e5ebc2703b3d284284b55caaff77c4bd8f1eb7dd
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-07-19 00:02:53 +0100

    Feat: Finalised deployment settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fitness4ever.wsgi:application
--- fitness4ever/settings.py
+++ fitness4ever/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-ewaa2f@u*ktuh-h_h50rdpg&v2i%i@508(g3&2=q56u@nswr*$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fitness4ever.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -116,7 +116,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -127,13 +127,17 @@ WSGI_APPLICATION = 'fitness4ever.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
