d7af9e0c6e5d184cd6dae945b32653e0344259a2
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-07-19 01:03:39 +0100

    Feat: moved all secret keys to heroku environment, reset all keys for robust security practise

--- fitness4ever/settings.py
+++ fitness4ever/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ewaa2f@u*ktuh-h_h50rdpg&v2i%i@508(g3&2=q56u@nswr*$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fitness4ever.herokuapp.com', 'localhost']
 
