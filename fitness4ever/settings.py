551e13ab75999b6300605252ffb9d97e16006c98
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-07-26 23:27:49 +0100

    Feat: fix media url

--- fitness4ever/settings.py
+++ fitness4ever/settings.py
@@ -201,8 +201,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Overide static and media URLS in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 #Stripe
 FREE_DELIVERY_THRESHOLD = 40
